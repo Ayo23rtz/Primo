493f4bb5f0333546162e795cd3d99a756bb2f0d5
Ayelen Miserez <ayo23rtz@gmail.com>
2022-01-21 23:51:52 -0300

    final del ejercicio- funcional

--- primo.py
+++ primo.py
@@ -1,18 +1,19 @@
+
 n = input('Ingrese un numero: ')
 while n.isdecimal() is False:
-    n = input('El digito ingresado no es un numero, por favor ingrese un numero: ')
+     n = input('El digito ingresado no es un numero, por favor ingrese un numero: ')
 
 n = int(n)
 
 
-def es_primo(n):
+def es_primo():
     if n < 2:
         return False
     else:
-        for elemento in range(2, n-1):
+        for elemento in range(2, n):
             if n % elemento == 0:
                 return False
         return True
-
-print(es_primo(n))
+        
+print(es_primo())
 
